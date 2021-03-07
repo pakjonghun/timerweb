533fb8634e7e1f5c9008f2be6508e3fc72c78901
정훈박 <jeonghunbag@jeonghunbag-ui-iMac.local>
2021-03-07 12:20:50 +0900

    optimization

--- src/Components/Button/ButtonContainer.js
+++ src/Components/Button/ButtonContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ButtonPresenter from "./ButtonPresenter";
 
+//버튼 컨테이너 props 중간 전달 역할 및 추가 기능추가용 컨테이너
 const Button = ({ text, onClick, isActive, className, endClick }) => {
   return (
     <ButtonPresenter
